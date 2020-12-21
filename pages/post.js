a9bc9b295185b1a00bfce2aa2b439e5fa623cb9f
Bhavesh <72858592+bhavesh92pandey@users.noreply.github.com>
2020-12-21 20:52:31 +0100

    Update post.js

--- pages/post.js
+++ pages/post.js
@@ -9,8 +9,9 @@ const PostPage = ({ post }) => (
 
 PostPage.getInitialProps = async ({ query }) => {
   const res = getPosts() // TODO: use query to fetch the blog page
+const blogArticle = res.filter(article => article.slug === query.slug)
 console.log(res[0])
-  return { post: res[0] }
+  return { post: blogArticle[0] }
 }
 
 export default PostPage
