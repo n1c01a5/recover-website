6aef2cf9ca0df0a9d60f53e65353d1097ff2d02d
Bhavesh <72858592+bhavesh92pandey@users.noreply.github.com>
2020-12-21 15:27:20 +0100

    Update index.js

--- api/posts/index.js
+++ api/posts/index.js
@@ -1,20 +1,20 @@
 export function getPosts() {
     return [
         {
-            slug: 'escrow-lost-found',
-            type: 'Anouncement',
-            title: 'Escrow 32423',
-            teaser: 'teaser',
-            cover: 'cover',
-            content: 'content'
+            slug: 'how-can-i-recover-my-phone-if-it-is-lost',
+            type: 'Lost-and-Found',
+            title: 'How-can-i-recover-my-phone-if-it-is-lost',
+            teaser: 'Lorem-ipsum',
+            cover: '<img.blog-2_lostphone.jpg>',
+            content: 'lorem-ipsum'
         },
         {
-            slug: 'escrow-lost-found-2',
-            type: 'Anouncement',
-            title: 'Escrow-2',
-            teaser: 'teaser',
-            cover: 'cover',
-            content: 'content'
+            slug: 'securing-valuables-with-escrow-smart-contracts',
+            type: 'Escrow',
+            title: 'securing-valuables-with-escrow-smart-contracts',
+            teaser: 'Lorem-ipsum',
+            cover: 'img.blog-1_escrow.jpg',
+            content: 'lorem-ipsum'
         }
     ]
   }
